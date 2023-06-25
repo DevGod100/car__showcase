ca6c3aad64eebf6369af3fc878ef29c8eb982c21
kiwicrypto1 <kiwicrypto1@protonmail.com>
2023-06-25 18:31:47 +0300

    first commit

---
+++ components/SearchBar.tsx
@@ -0,0 +1,154 @@
+'use client'
+import Image from 'next/image'
+import {SearchManufacturer} from './'
+
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+const SearchButton = ({otherClasses}: {otherClasses: string}) => (
+  <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+   <Image 
+   src='/magnifying-glass.svg'
+   alt='search'
+   width={40}
+   height={40}
+   className='object-contain'
+   />
+  </button>
+)
+
+const SearchBar = ({setManufacturer, setModel}) => {
+  const [searchManufacturer, setSearchManufacturer] = useState('')
+  const [searchModel, setSearchModel] = useState('')
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) =>{
+      e.preventDefault();
+
+      if(searchManufacturer === '' && searchModel === ''){
+        return alert('Please add a filter to the search')
+      }
+      setModel(searchModel)
+      setManufacturer(searchManufacturer)
+  }
+
+  
+  return (
+    <form className='searchbar' onSubmit={handleSearch}>
+      <div className='searchbar__item'>
+        <SearchManufacturer 
+        selected={searchManufacturer}
+        setSelected={setSearchManufacturer}
+        />
+        <SearchButton otherClasses='sm:hidden'/>
+      </div>
+      <div className='searchbar__item'>
+       <Image 
+       src='/model-icon.png'
+       alt='modek'
+       width={25}
+       height={25}
+       className='absolute w-[20px] h-[20px] ml-4'
+       />
+       <input 
+       type="text"
+       name='model'
+       value={searchModel} 
+       onChange={(e) => setSearchModel(e.target.value)}
+       placeholder='Tiguan'
+       className='searchbar__input'
+       />
+       <SearchButton otherClasses='sm:hidden'/>
+      </div>
+      <SearchButton otherClasses='max-sm:hidden'/>
+    </form>
+  )
+}
+
+export default SearchBar
+
+// 'use client'
+// import Image from 'next/image'
+// import {SearchManufacturer} from './'
+
+// import React, { useState } from 'react'
+// import { useRouter } from 'next/navigation'
+
+// const SearchButton = ({otherClasses}: {otherClasses: string}) => (
+//   <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+//    <Image 
+//    src='/magnifying-glass.svg'
+//    alt='search'
+//    width={40}
+//    height={40}
+//    className='object-contain'
+//    />
+//   </button>
+// )
+
+// const SearchBar = () => {
+//   const [manufacturer, setManufacturer] = useState('')
+//   const [model, setModel] = useState('')
+//   const router = useRouter();
+
+//   const handleSearch = (e: React.FormEvent<HTMLFormElement>) =>{
+//       e.preventDefault();
+
+//       if(manufacturer === '' && model === ''){
+//         return alert('Please add a filter to the search')
+//       }
+//       updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase())
+//   }
+
+//   const updateSearchParams = (model: string, manufacturer: string) => {
+//     const SearchParams = new URLSearchParams(window.location.search);
+
+//     if (model) {
+//       SearchParams.set('model', model)
+//     } else {
+//         SearchParams.delete('model')
+//       }
+
+//       if (manufacturer) {
+//         SearchParams.set('manufacturer', manufacturer)
+//       } else {
+//           SearchParams.delete('manufacturer')
+//         }
+
+//         const newPathname = `${window.location.pathname}?${SearchParams.toString()}`
+
+//         router.push(newPathname)
+//     }
+//   return (
+//     <form className='searchbar' onSubmit={handleSearch}>
+//       <div className='searchbar__item'>
+//         <SearchManufacturer 
+//         manufacturer={manufacturer}
+//         setManufacturer={setManufacturer}
+//         />
+//         <SearchButton otherClasses='sm:hidden'/>
+//       </div>
+//       <div className='searchbar__item'>
+//        <Image 
+//        src='/model-icon.png'
+//        alt='modek'
+//        width={25}
+//        height={25}
+//        className='absolute w-[20px] h-[20px] ml-4'
+//        />
+//        <input 
+//        type="text"
+//        name='model'
+//        value={model} 
+//        onChange={(e) => setModel(e.target.value)}
+//        placeholder='Tiguan'
+//        className='searchbar__input'
+//        />
+//        <SearchButton otherClasses='sm:hidden'/>
+//       </div>
+//       <SearchButton otherClasses='max-sm:hidden'/>
+//     </form>
+//   )
+// }
+
+// export default SearchBar
